4f90698a4c8b91b3c7557db6687a5eb6b43c6f02
carloscarvalho <misterchaty@outlook.com>
2026-05-28 17:59:16 -0300

    Remove Unused Classes

--- src/main/java/br/com/pimentaestetica/crm/model/user/UserController.java
+++ src/main/java/br/com/pimentaestetica/crm/model/user/UserController.java
@@ -4,9 +4,6 @@ import br.com.pimentaestetica.crm.model.appoinment.Appointment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Set;
-
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
